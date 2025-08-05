0ce32e8218926adcb4adaadb3d232a2e9aee9fa8
Luis <luisangeltellez30@gmail.com>
2025-08-04 22:28:47 -0600

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/angelTe30.js
@@ -0,0 +1,28 @@
+
+//url de la documentación de JavaScript
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//Comentario utilizando barra doble
+/*  Este es un comentario utlizando barra y asterisco
+    con este se pueden hacer comentarios en varias lineas
+*/
+
+let num = 10; //Una variable declarada con let, se utiliza cuando el valor puede cambiar
+const a = 5 //Una variable declarada con const, se utuiliza cuando el  valor no cambia
+
+let string = "¡Hola Mundo!"; //Variable para cadena de texto
+
+let number = 20; //Variable para un número entero
+
+let status = true; //Variable para un valor booleano, ya sea true o false
+
+let variable; //Variable sin valor, esto dará resultado a undefined
+
+let bigInit = 9007199254740992n //Variable para un numero entero grante, donde se exceden los limites de number
+
+let vacio = null; //Variable sin valor, pero con el tipo asignado null
+
+let simbolo = Symbol("Identrificador"); //Variable que representa un valor único
+
+
+console.log("¡Hola. JavaScript!");
